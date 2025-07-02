92e25f8594c1c3be7746cdfc95d8b16d885aa423
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2025-07-02 17:47:35 +0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "F-Portfolio",
+  icons: {
+    icon: [
+      {
+        url: "/ChatGPT Image Jul 2, 2025, 05_31_45 PM.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
