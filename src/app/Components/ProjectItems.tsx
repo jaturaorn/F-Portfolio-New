a1e6078ba8ce9330dd6f138e437cecc16b7cd29d
jaturaporn <jaturapornff@gmail.com>
2025-07-08 22:03:19 +0700

    finish my work

--- src/app/Components/ProjectItems.tsx
+++ src/app/Components/ProjectItems.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 
 interface ProjectItemsProps {
   projectDataObject: {
     title: string;
-    photoName: string;
+    logo: string;
     link: string;
   };
 }
@@ -11,8 +12,10 @@ interface ProjectItemsProps {
 const ProjectItems = ({ projectDataObject }: ProjectItemsProps) => {
   return (
     <Link href={projectDataObject.link} target="_blank" className="project-card">
-    <div>
-      <div className="project-image project-web" />
+    <div className="">
+      <div className="project-image project-web" >
+        <Image src={projectDataObject.logo} width={200} height={200} alt="" className=" size-[55px] rounded-[15px]" />
+      </div>
       <div className="project-content">
         <h3>{projectDataObject.title}</h3>
         <p></p>
--- src/app/project.ts
+++ src/app/project.ts
@@ -1,68 +1,108 @@
 const projectData = [
   {
     title: "eat-n-spilt",
-    link: "https://eat-n-spilt.vercel.app/",
+    link: "https://eat-n-spilt.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "ecom-website",
-    link: "https://ecom-website-seven.vercel.app/",
+    link: "https://ecom-website-seven.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "Tindog-Project",
     link: "https://fabulous-biscochitos-86b7e2.netlify.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "Colma-academy-project",
-    link: "https://coma-academy.netlify.app/",
+    link: "https://coma-academy.netlify.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "Count-project",
-    photoName: "/public/Images/Capture3.jpg",
-    link: "https://beamish-blancmange-f95b08.netlify.app/",
+    logo: "/Logos/MyLogo.png",
+    link: "https://beamish-blancmange-f95b08.netlify.app",
   },
   {
     title: "Drum-kit-Project",
-    link: "https://drum-f-0c4a41.netlify.app/",
+    link: "https://drum-f-0c4a41.netlify.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "Tech_Ed-Project",
-    link: "https://freetime-project.vercel.app/",
+    link: "https://freetime-project.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "MEME-Project",
-    link: "https://meme-ws-jaturaorn.vercel.app/",
+    link: "https://meme-ws-jaturaorn.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "Mondrian-Project",
-    link: "https://mondrian-project-alpha.vercel.app/",
+    link: "https://mondrian-project-alpha.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "shopping-cart",
     link: "https://shopping-cartnew-nine.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   {
     title: "pizza-m-enu",
-    link: "https://pizza-m-enu.vercel.app/",
+    link: "https://pizza-m-enu.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   { 
     title: "Car-Land-Project",
     link: "https://learn-next-js-tau-ten.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   { 
     title: "Travel-lists-Project",
     link: "https://travel-list-react-green.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   { 
     title: "Calculator",
     link: "https://calculator-js-five-eta.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   { 
     title: "NBA Player stat",
     link: "https://nbastat-flame.vercel.app",
+    logo: "/Logos/MyLogo.png"
   },
   { 
     title: "Steps-Project",
-    link: "https://steps-react-eight.vercel.app/",
+    link: "https://steps-react-eight.vercel.app",
+    logo: "/Logos/MyLogo.png"
+  },
+  { 
+    title: "Moon Mission SocialFi Platform",
+    link: "https://evermoon-social-fi-mainnet.vercel.app",
+    logo: "/Logos/EVM.webp"
+  },
+  { 
+    title: "Evermoon Web Application",
+    link: "https://evermoon-website-2024.vercel.app",
+    logo: "/Logos/EVM.webp"
+  },
+  { 
+    title: "Axolt Slap Trading Platform",
+    link: "https://axolt-slap.vercel.app",
+    logo: "/Logos/EVM.webp"
+  },
+  { 
+    title: "IAI.CENTER AI Token Platform",
+    link: "https://evermoon-vesting.vercel.app",
+    logo: "/Logos/mock-iai-token.png"
+  },
+  { 
+    title: "Evermoon Vesting Platform",
+    link: "https://evermoon-vesting.vercel.app",
+    logo: "/Logos/EVM.webp"
   },
 ];
 
